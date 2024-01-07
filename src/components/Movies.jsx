292469d5631a8d3d0c6fa22dcf4d2d6e99ccaaf8
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-01-07 13:52:16 +0100

    basics for Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://movie-app-gx5p.onrender.com/api/movies")
+      .then(res => setMovies(res.data))
+      .catch(e => console.error(e));
+  }, []);
+
+
   return (
-    <section>Movies</section>
-    //Sabina Part
+    <div>
+      <h2>Movies</h2>
+      <ul>
+        {Array.isArray(movies) && movies.length > 0 ? (movies.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              {movie.title}
+            </Link>
+          </li>
+        ))
+        ) : (
+          <li>No movies available</li>
+        )}
+      </ul>
+
+    </div>
+    
   );
 };
 
