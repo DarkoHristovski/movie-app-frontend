e926316f796ab0a9dfb85f1e82149a2cbf2cfd11
borabey <bora.b.sarikaya@gmail.com>
2024-01-12 17:06:26 +0200

    add new component BestRate

---
+++ src/components/BestRate.jsx
@@ -0,0 +1,53 @@
+
+import React from 'react';
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+import MovieCard from "./MovieCard";
+import MovieCarousel from "./MovieCarousel";
+
+const BestRate = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://movie-app-gx5p.onrender.com/api/movies")
+      .then((res) => {
+        const filteredMovies = res.data.filter((movie) => movie.rating > 8);
+        setMovies(filteredMovies);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container mx-auto p-8">
+      <h3 className="text-xl font-bold mb-3">Popular movies</h3>
+      <MovieCarousel movies={movies} className="w-full" />
+      <h3 className="text-xl font-bold mb-3">Explore movies</h3>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 mx-4">
+        {Array.isArray(movies) && movies.length > 0 ? (
+          movies.map((movie) => (
+            <div key={movie.id}>
+              <MovieCard movie={movie} />
+            </div>
+          ))
+        ) : (
+          <div>No movies available</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BestRate;
